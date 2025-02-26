61661bef5fc8c2277f391cbf69fcca293b637de2
Tiago Dinis <tiagodinis33@proton.me>
2025-02-26 14:13:52 +0000

    feat: start working on the nix os module

--- flake.nix
+++ flake.nix
@@ -4,45 +4,18 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    snowfall-lib = {
+        url = "github:snowfallorg/lib";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+  outputs = inputs:
+        inputs.snowfall-lib.mkFlake {
+            inherit inputs;
+            src = ./.;
+            alias.packages.default = "tibs";
+            alias.modules.nixos.default = "tibs";
         };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell rec {
-          # OVMF_PATH = "${pkgs.OVMF.fd}/FV/OVMF.fd";
-          # AAVMF_PATH = "${pkgs.OVMF.fd}/FV/AAVMF.fd";
-          buildInputs = [
-            # qemu_full
-            # xorriso
-            # gnumake
-            # git
-            mesa
-            libGL
-            cmake
-            glfw
-            pkg-config
-            xorg.libX11
-            xorg.libXrandr
-            xorg.libXinerama
-            xorg.libXcursor
-            xorg.libXi
-            python3
-            ninja
-            fontconfig
-            freetype
-            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-          ];
-          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-        };
-      }
-    );
 }
