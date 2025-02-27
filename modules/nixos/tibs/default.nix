2671a713ad69b2d916f66a21bd1ec0a5d87bb7af
Tiago Dinis <tiagodinis33@proton.me>
2025-02-27 11:04:17 +0000

    feat: silent boot

--- modules/nixos/tibs/default.nix
+++ modules/nixos/tibs/default.nix
@@ -28,15 +28,23 @@ in {
         message = "Conflict: Plymouth is enabled. Please disable Plymouth when TIBS is enabled.";
       }
     ];
-    boot.kernelParams = mkForce ["quiet" "loglevel=0" "systemd.show_status=0"];
-        
-    boot.postBootCommands = ''
-      echo "Iniciando TIBS..."
-      export OPENGL_DRIVER_PATH=${driversEnv}
-      echo "OPENGL_DRIVER_PATH=$OPENGL_DRIVER_PATH"
-      ln -sfn $OPENGL_DRIVER_PATH /run/opengl-driver
-      ls /run/opengl-driver/
-      LD_LIBRARY_PATH="${lib.getLib pkgs.libGL}/lib" ${tibs}/bin/tibs &
-    '';
+    boot.kernelParams = mkForce ["quiet" "loglevel=0" "systemd.show_status=0" "udev.log_level=3" "vt.global_cursor_default=0"];
+    systemd.services.tibs = {
+      description = "Tiago's Incredible Boot Screen";
+      wantedBy = [ "sysinit.target" ];
+      serviceConfig = {
+        Type = "forking";
+        ExecStart = pkgs.writeShellScript "tibs-service" ''
+          echo "Iniciando TIBS..."
+          export OPENGL_DRIVER_PATH=${driversEnv}
+          echo "OPENGL_DRIVER_PATH=$OPENGL_DRIVER_PATH"
+          ln -sfn $OPENGL_DRIVER_PATH /run/opengl-driver
+          ls /run/opengl-driver/
+          LD_LIBRARY_PATH="${lib.getLib pkgs.libGL}/lib" ${tibs}/bin/tibs
+        '';
+      };
+    };
+    boot.consoleLogLevel = 0;
+    boot.initrd.systemd.enable = true;    
   };
 }
